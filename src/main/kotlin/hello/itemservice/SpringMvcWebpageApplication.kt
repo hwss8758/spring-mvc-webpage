60c4b64d79262e4e4166f6176c41906605646631
wonsang.hwang <wonsang.hwang@navercorp.com>
2021-09-28 21:14:55 +0900

    first commit

---
+++ src/main/kotlin/hello/itemservice/SpringMvcWebpageApplication.kt
@@ -0,0 +1,11 @@
+package hello.itemservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringMvcWebpageApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringMvcWebpageApplication>(*args)
+}
